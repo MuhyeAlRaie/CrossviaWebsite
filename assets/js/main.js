6866c213807e60d43684358a0b1fd4083a11b871
Muhye aldeen <86913045+MuhyeAlRaie@users.noreply.github.com>
2024-11-07 23:17:54 +0300

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Initialize AOS library for animations
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init({
+      duration: 1200, // Animation duration
+      once: true      // Whether animation should happen only once
+    });
+  });
